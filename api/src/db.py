44b42c5afe5637860f2d0d08fe0a676ebc068c09
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-06-02 15:37:12 +0200

    Allows retrieving Postgres connections

--- api/src/db.py
+++ api/src/db.py
@@ -5,21 +5,60 @@ import time
 import uuid
 
 import redis as r
+import sqlalchemy as al
 
 import settings as s
-    
-def get_connection(settings: s.Settings, username: str) -> r.Redis:
-    return r.Redis(
-        host=settings.redis_host, 
-        port=settings.redis_port, 
-        db=settings.redis_db,
-        username=username,
-        password=Path(f'/run/secrets/redis_{username}').read_text(),
-        decode_responses=True,
-    )
+
+_session_makers: dict[str, al.orm.sessionmaker] = {}
 
 class DataBaseError(Exception):
     pass
+    
+def get_redis_connection(settings: s.Settings, username: str) -> r.Redis:
+    try:
+        return r.Redis(
+            host=settings.redis_host, 
+            port=settings.redis_port, 
+            db=settings.redis_db,
+            username=username,
+            password=Path(f'/run/secrets/redis_{username}').read_text().strip(),
+            decode_responses=True,
+        )
+    except FileNotFoundError as e:
+        raise DataBaseError(f'No credentials for Redis user {username}') from e
+    
+async def get_postgres_connection(settings: s.Settings, username: str) -> al.ext.asyncio.AsyncSession:
+    global _session_makers
+    
+    if username in _session_makers:
+        session_maker = _session_makers['username']
+    else:
+        try:
+            password = Path(f'/run/secrets/postgres_{username}').read_text().strip()
+        except FileNotFoundError as e:
+            raise DataBaseError(f'No credentials for Postgres user {username}') from e
+        
+        engine = al.ext.asyncio.create_async_engine(
+            f'postgresql+asyncpg://{username}:{password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}',
+            pool_size=settings.postgres_pool_size,
+            max_overflow=settings.postgres_max_overflow,
+        )
+        
+        session_maker = al.orm.sessionmaker(
+            bind=engine,
+            class_=al.ext.asyncio.AsyncSession,
+            expire_on_commit=False,
+        )
+        
+        _session_makers['username'] = session_maker
+    
+    async with session_maker() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception as e:
+            await session.rollback()
+            raise DataBaseError('Database operation failed') from e
 
 def handle_redis_errors(func):
     @functools.wraps(func)
@@ -55,7 +94,7 @@ def retry_on_error(retries: int, delay_s: float, exceptions: Type[BaseException]
 def create_session(settings: s.Settings, data: str) -> str:
     session_id = str(uuid.uuid4())
     
-    connection = get_connection(settings, 'write')
+    connection = get_redis_connection(settings, 'write')
     connection.setex(session_id, settings.session_ttl, data)
 
     return session_id
@@ -63,7 +102,7 @@ def create_session(settings: s.Settings, data: str) -> str:
 @handle_redis_errors
 @retry_on_error(1, 2, (r.ConnectionError, r.TimeoutError))
 def get_session(settings: s.Settings, session_id: str) -> str | None:
-    connection = get_connection(settings, 'read_delete')
+    connection = get_redis_connection(settings, 'read_delete')
     
     pipe = connection.pipeline()
     pipe.get(session_id)
