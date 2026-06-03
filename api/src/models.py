d5d0e900d7a500eafb49246b33eb50e34d72f83d
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-06-03 15:17:08 +0200

    Defines an Account model

---
+++ api/src/models.py
@@ -0,0 +1,24 @@
+import enum
+
+from sqlalchemy import String, LargeBinary, Enum
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class AccountStatus(str, enum.Enum):
+    enabled = 'enabled'
+    disabled = 'disabled'
+    unverified = 'unverified'
+
+class Account(Base):
+    __tablename__ = 'accounts'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    display_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    mail: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    password_hash: Mapped[bytes] = mapped_column(LargeBinary, nullable=True)
+    status: Mapped[AccountStatus] = mapped_column(
+        Enum(AccountStatus, name='account_status'),
+        default=AccountStatus.unverified
+    )
