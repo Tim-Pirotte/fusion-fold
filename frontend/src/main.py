a52f008783c19e715ff0e0986dfd3f5d6aa55774
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-06-03 16:52:49 +0200

    Adds a route to the frontend to verify accounts

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -6,5 +6,9 @@ app = fl.Flask(__name__)
 def index():
     return fl.render_template('index.html')
 
+@app.route('/verify-account/<string:token>')
+def verify_account(**_):
+    return fl.render_template('verify_account.html')
+
 if __name__ == '__main__':
     app.run(debug=False, threaded=True)
