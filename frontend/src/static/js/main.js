75b48f435c6106bc259b34fa4cdb14878de3425c
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-06-07 15:32:15 +0200

    Redirects the user when not logged in

--- frontend/src/static/js/main.js
+++ frontend/src/static/js/main.js
@@ -1,5 +1,13 @@
 import * as v from "./viewer.js";
 import * as foldingUI from "./folding/ui.js";
 
+const loggedIn = document.cookie
+    .split("; ")
+    .some(c => c.startsWith("logged_in="));
+
+if (!loggedIn) {
+    location.href = "/login";
+}
+
 const objectManager = v.init();
 foldingUI.init(objectManager);
