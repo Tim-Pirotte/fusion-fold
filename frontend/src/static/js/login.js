1e46153aef1f2bcbf6c7c232faa1b48a55a07e98
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-06-07 16:21:31 +0200

    Connects the login API to the frontend

--- frontend/src/static/js/login.js
+++ frontend/src/static/js/login.js
@@ -1,6 +1,11 @@
+import { API } from "./config.js";
+
 function init() {
     document.querySelectorAll("input[name='toggle']")
         .forEach($i => $i.addEventListener("change", toggleForm));
+
+    document.getElementById("login").addEventListener("submit", login);
+    document.getElementById("register").addEventListener("submit", register);
 }
 
 init();
@@ -16,4 +21,35 @@ function toggleForm() {
         $login.classList.add("hidden");
         $register.classList.remove("hidden");
     }
+}
+
+async function login(e) {
+    e.preventDefault();
+
+    const mail = document.getElementById("mail").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch(
+        `${API}/v1/accounts/login`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                mail: mail,
+                password: password,
+            }),
+        },
+    );
+
+    if (!res.ok) {
+        alert("Incorrect e-mail and/or password");
+    } else {
+        location.href = "/";
+    }
+}
+
+function register(e) {
+    e.preventDefault();
 }
--- frontend/src/static/js/verify.js
+++ frontend/src/static/js/verify.js
@@ -24,7 +24,7 @@ function completeAccount(e) {
             },
             body: JSON.stringify({
                 password,
-            })
+            }),
         },
     );
 }
