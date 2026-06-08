fdcee38fd35f9e10573acda415fd716f4823468c
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-06-08 22:45:34 +0200

    Allows loading a sequence from history

--- frontend/src/static/js/folding/history.js
+++ frontend/src/static/js/folding/history.js
@@ -5,6 +5,9 @@ function init(sidePanel) {
 
     document.getElementById("show-history")
         .addEventListener("click", (_) => toggleHistory(sidePanel));
+
+    document.getElementById("history")
+        .addEventListener("click", (e) => handleHistoryClick(e, sidePanel))
 }
 
 function toggleHistory(sidePanel) {
@@ -53,11 +56,83 @@ async function renderHistory() {
     const $template = document.getElementById("prediction-template");
     const $history = document.getElementById("history");
 
+    $history.querySelectorAll(":scope > :not(template)")
+        .forEach(el => el.remove());
+
     for (const prediction of data.predictions) {
+        const date = new Date(prediction.created_at);
+
+        const formatted = date.toLocaleString(undefined, {
+            dateStyle: 'medium',
+            timeStyle: 'short',
+            timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        });
+
         const $li = $template.content.cloneNode(true).querySelector("li");
+
+        $li.dataset.id = prediction["id"];
+        $li.querySelector("span").textContent = `${formatted} ${prediction["display_name"]}`;
 
         $history.appendChild($li);
     }
+}
+
+function handleHistoryClick(e, sidePanel) {
+    const button = e.target.closest("button");
+
+    if (!button) {
+        return;
+    }
+
+    const id = button.closest("li").dataset.id;
+
+    if (button.classList.contains("load-sequence")) {
+        loadSequence(id);
+        sidePanel.back();
+    } else {
+        loadCoordss(id);
+    }
+}
+
+async function loadSequence(id) {
+    let res;
+
+    try {
+        res = await fetch(
+            `${API}/v1/predictions/${id}/sequence`,
+            {
+                method: "GET",
+                credentials: "include",
+            },
+        );
+    } catch (e) {
+        console.error(e);
+        alert("Something unknown went wrong");
+
+        return;
+    }
+
+    if (!res.ok) {
+        if (res.status === 404) {
+            alert("The logged in account does not exist anymore");
+
+            return;
+        } else if (res.status === 401) {
+            location.href = "/login";
+        } else {
+            alert("Something went wrong while retrieving history data");
+
+            return;
+        }
+    }
+
+    const data = await res.json();
+
+    document.getElementById("sequence").value = data["sequence"];
+}
+
+async function loadCoordss(id) {
+
 }
 
 export { init };
