09745627a061a1f4cd0b62b0932918c2b87012ea
Tim Pirotte <143127317+Tim-Pirotte@users.noreply.github.com>
2026-06-09 19:40:25 +0200

    Loads the API base url in the frontend from the environment

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,26 +1,29 @@
 import flask as fl
 
+import settings as s
+
 app = fl.Flask(__name__)
+settings = s.Settings()
 
 @app.route('/')
 def index():
-    return fl.render_template('index.html')
+    return fl.render_template('index.html', api=settings.api)
 
 @app.route('/login')
 def login():
-    return fl.render_template('login.html')
+    return fl.render_template('login.html', api=settings.api)
 
 @app.route('/verify-account/<string:token>')
 def verify_account(**_):
-    return fl.render_template('verify_account.html')
+    return fl.render_template('verify_account.html', api=settings.api)
 
 @app.route('/reset-password')
 def reset_password():
-    return fl.render_template('reset_password.html')
+    return fl.render_template('reset_password.html', api=settings.api)
 
 @app.route('/reset-password/<string:token>')
 def complete_password_reset(**_):
-    return fl.render_template('complete_password_reset.html')
+    return fl.render_template('complete_password_reset.html', api=settings.api)
 
 if __name__ == '__main__':
     app.run(debug=False, threaded=True)
